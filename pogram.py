ab0af8fa11d6f2f8360f8659f35c4239e9fca2b5
simonostensson2001 <simonostensson2001@users.noreply.github.com>
2019-10-15 11:04:20 +0200

    lektion 1 avslut

--- pogram.py
+++ pogram.py
@@ -13,25 +13,45 @@ while gamemode == 0:
 
 
 
-        player1 = input("Sten,Sax,Pase")
-        player2 = input("Sten,Sax,Pase")
+        player1 = input("Sten,Sax,Påse")
+        player2 = input("Sten,Sax,Påse")
     elif gamemode == 2:
         print("Du har valt att spela mot pc")
         sax = 1
         sten = 2
         pase = 3
-        player1 = input("[1]sax, [2]sten, [3]pase")
+        player1 = input("[1]sax, [2]sten, [3]påse: ")
         while player1 == 1:
             pcchoice = input(random.randint(1, 3))
             if pcchoice == 1:
                 print("oavgjort")
             elif pcchoice == 2:
-                print("forlust")
+                print("förlust")
             elif pcchoice == 3:
                 print("vinst")
             else:
                 player1 = 1
 
+        while player1 == 2:
+            pcchoice = input(random.randint(1, 3))
+            if pcchoice == 1:
+                print("förlust")
+            elif pcchoice == 2:
+                print("oavgjort")
+            elif pcchoice == 3:
+                print("vinst")
+            else:
+                player1 = 2
 
+        while player1 == 3:
+            pcchoice = input(random.randint(1, 3))
+            if pcchoice == 1:
+                print("förlust")
+            elif pcchoice == 2:
+                print("vinst")
+            elif pcchoice == 3:
+                print("oavgjort")
+            else:
+                player1 = 1
     else:
         gamemode = 0
