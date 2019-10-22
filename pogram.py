b53edf62dbcb184e0ee49593335699ccb1490f7b
simonostensson2001 <simonostensson2001@users.noreply.github.com>
2019-10-22 09:13:03 +0200

    Lagt till ett poängsystem

--- pogram.py
+++ pogram.py
@@ -1,21 +1,21 @@
 import random
 import time # Används vid olika moment, Delay
 print ("Välkommen till vårat Sten,Sax,Pase pogram")
-
-
-
+PcScore = 0
+Player1Score = 0
+Player2Score = 0
 
 gamemode = 0
 
 while gamemode == 0:
-    gamemode = int(input("[1]Spela mot en kompis [2]Spela mot dator"))
+    gamemode = int(input("[1]Spela mot en kompis [2]Spela mot dator: "))
     if gamemode == 1:
         print("Du har valt att spela 1v1")
 
 
 
-        player1 = input("Sten,Sax,Påse")
-        player2 = input("Sten,Sax,Påse")
+        player1 = input("[1]sax, [2]sten, [3]påse: ")
+        player2 = input("[1]sax, [2]sten, [3]påse: ")
     elif gamemode == 2:
         print("Du har valt att spela mot pc")
         sax = 1
@@ -25,44 +25,90 @@ while gamemode == 0:
         pcchoice = int(random.randint(1, 3))
         while player1 == 1:
             if pcchoice == 1:
+                print("Du spelade sax")
+                time.sleep(1)
+                print("Datorn spelade sax")
+                time.sleep(1)
                 print("oavgjort")
+                print("Spelare", Player1Score, "-", PcScore, "Dator",)
                 player1 = 0
             elif pcchoice == 2:
+                print("Du spelade sax")
+                time.sleep(1)
+                print("Datorn spelade sten")
+                time.sleep(1)
                 print("förlust")
+                PcScore = PcScore + 1
+                print("Spelare", Player1Score, "-", PcScore, "Dator",)
                 player1 = 0
             elif pcchoice == 3:
+                print("Du spelade sax")
+                time.sleep(1)
+                print("Datorn spelade påse")
+                time.sleep(1)
                 print("vinst")
+                Player1Score = Player1Score + 1
+                print("Spelare", Player1Score, "-", PcScore, "Dator",)
                 player1 = 0
             else:
                 player1 = 0
 
         while player1 == 2:
             if pcchoice == 1:
-                print("förlust")
+                print("Du spelade sten")
+                time.sleep(1)
+                print("datorn spelade sax")
+                time.sleep(1)
+                print("vinst")
                 player1 = 0
             elif pcchoice == 2:
+                print("Du spelade sten")
+                time.sleep(1)
+                print("Datorn spelade sten")
+                time.sleep(1)
                 print("oavgjort")
                 player1 = 0
             elif pcchoice == 3:
-                print("vinst")
+                print("Du spelade sten")
+                time.sleep(1)
+                print("Datorn spelade påse")
+                time.sleep(1)
+                print("förlust")
                 player1 = 0
             else:
                 player1 = 0
 
         while player1 == 3:
             if pcchoice == 1:
+                print("Du spelade påse")
+                time.sleep(1)
+                print("Datorn spelade sax")
+                time.sleep(1)
                 print("förlust")
                 player1 = 0
             elif pcchoice == 2:
+                print("Du spelade påse")
+                time.sleep(1)
+                print("Datorn spelade sten")
+                time.sleep(1)
                 print("vinst")
                 player1 = 0
             elif pcchoice == 3:
+                print("Du spelade påse")
+                time.sleep(1)
+                print("Datorn spelade påse")
+                time.sleep(1)
                 print("oavgjort")
                 player1 = 0
             else:
                 player1 = 0
         else:
             time.sleep(1)
-            print("Spela igen!")
-            time.sleep(1)
-            gamemode = 0
+            if PcScore == 2:
+                exit()
+            elif Player1Score == 2:
+                exit()
+            else:
+                print("Spela igen!")
+                time.sleep(1)
+                gamemode = 0
